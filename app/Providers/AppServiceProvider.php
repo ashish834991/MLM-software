d6547425df331fe82b2f33ef96f6afe34046e462
Ashish pal <ashish834991@gmail.com>
2026-05-10 14:02:15 +0530

    fixed railway deploy issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,50 +31,36 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+{
+    try {
+
+        if (!\Schema::hasTable('general_settings') || !\Schema::hasTable('languages')) {
+            return;
+        }
+
         $general = gs();
         $activeTemplate = activeTemplate();
+
         $viewShare['general'] = $general;
         $viewShare['activeTemplate'] = $activeTemplate;
         $viewShare['activeTemplateTrue'] = activeTemplate(true);
         $viewShare['language'] = Language::all();
         $viewShare['emptyMessage'] = 'Data not found';
-        $viewShare['pages'] = Page::where('tempname',$activeTemplate)->where('is_default', Status::NO)->get();
+
+        $viewShare['pages'] = Page::where('tempname', $activeTemplate)
+            ->where('is_default', Status::NO)
+            ->get();
+
         view()->share($viewShare);
-
-
-        view()->composer('admin.partials.sidenav', function ($view) {
-            $view->with([
-                'bannedUsersCount'           => User::banned()->count(),
-                'emailUnverifiedUsersCount' => User::emailUnverified()->count(),
-                'mobileUnverifiedUsersCount'   => User::mobileUnverified()->count(),
-                'kycUnverifiedUsersCount'   => User::kycUnverified()->count(),
-                'kycPendingUsersCount'   => User::kycPending()->count(),
-                'pendingTicketCount'         => SupportTicket::whereIN('status', [Status::TICKET_OPEN, Status::TICKET_REPLY])->count(),
-                'pendingDepositsCount'    => Deposit::pending()->count(),
-                'pendingWithdrawCount'    => Withdrawal::pending()->count(),
-            ]);
-        });
-
-        view()->composer('admin.partials.topnav', function ($view) {
-            $view->with([
-                'adminNotifications' => AdminNotification::where('is_read', Status::NO)->with('user')->orderBy('id', 'desc')->take(10)->get(),
-                'adminNotificationCount' => AdminNotification::where('is_read', Status::NO)->count(),
-            ]);
-        });
-
-        view()->composer('partials.seo', function ($view) {
-            $seo = Frontend::where('data_keys', 'seo.data')->first();
-            $view->with([
-                'seo' => $seo ? $seo->data_values : $seo,
-            ]);
-        });
 
         if ($general->force_ssl) {
             \URL::forceScheme('https');
         }
 
+    } catch (\Exception $e) {
 
-        Paginator::useBootstrapFour();
     }
+
+    Paginator::useBootstrapFour();
+}
 }
